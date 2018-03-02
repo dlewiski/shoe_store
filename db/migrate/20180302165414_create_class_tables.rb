4cdc98175e6f41104ad9dca055c70e7d5d2f025d
David Lewis <davidlewisp@gmail.com>
2018-03-02 09:14:16 -0800

    add class tables and join table to database

--- db/migrate/20180302165414_create_class_tables.rb
+++ db/migrate/20180302165414_create_class_tables.rb
@@ -1,4 +1,17 @@
 class CreateClassTables < ActiveRecord::Migration[5.1]
   def change
+    create_table(:stores) do |t|
+     t.column(:store, :string)
+    end
+
+    create_table(:brands) do |t|
+     t.column(:brand, :string)
+     t.column(:price, :decimal)
+    end
+
+    create_table(:brands_stores) do |t|
+     t.column(:brand, :string)
+     t.column(:price, :decimal)
+    end
   end
 end
